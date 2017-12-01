3b3096031c57d454cb5404acee9eb4881b242121
rkk <zhhailon@gmail.com>
2017-12-01 13:50:05 -0500

    add rop support

---
+++ sdk/trts/rop.cpp
@@ -0,0 +1,57 @@
+// #include "../Enclave.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+#define USE(x) x=x;
+
+void foo() {
+    uint64_t i = 2;
+    asm("movq %0, %%rbx" :: "r"(i));
+    // register int myrbx asm("rbx");
+    // printf("rbx = 0x%x\n", myrbx);
+}
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+    // rdi: 1: rax, 2: rbx, 0: others
+    int which_reg = 0;
+    char *shadowstack = 0;
+    char *shadowrsp = 0;
+
+    void checkheapexistence()
+    {
+        if(shadowstack == 0)
+        {
+            shadowstack = (char*)malloc(1000);
+            shadowrsp = shadowstack;
+        }
+    }
+
+    void checkrsp()
+    {
+
+    }
+
+    void heap_stack_push(int a) {
+        USE(a)
+        // printf("push\n");
+        // register int myrsp asm("rsp");
+        // printf("0x%x - 0x%x = 0x%x\n", a, myrsp, a - myrsp);
+        checkheapexistence();
+    }
+
+    void heap_stack_pop(int a) {
+        USE(a)
+        // printf("pop\n");
+        // register int myrsp asm("rsp");
+        // printf("0x%x - 0x%x = 0x%x\n", a, myrsp, a - myrsp);
+        checkheapexistence();
+        checkrsp();
+        foo();
+    }
+
+#if defined(__cplusplus)
+}
+#endif
